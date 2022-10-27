b07c93437021b3aeeb79cf3bf4b3bb6f8dd84807
valmri <valmarmie87@outlook.com>
2022-10-27 17:40:03 +0200

    Ajout map

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import RequeteContrat from './RequeteContrat.js';
 import RequeteStation from './RequeteStation.js';
+import ListeContrats from './ListeContrats';
 
 
 function App() {
   return (
-    <div>
-      <RequeteContrat></RequeteContrat>
+    <div className='app'>
+      <aside className='menu'>
+        <h1 className='titre'>Stations vélos</h1>
+        <ListeContrats></ListeContrats>
+      </aside>
+      <div className='map'></div>
     </div>
   );
 }
---
+++ src/Map.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Map.css';
+import { MapContainer, TileLayer, useMap } from 'react-leaflet'
+
+
+class Map extends React.Component {
+
+    render() {
+        return (
+            <div className="map">
+
+            </div>
+        );
+    }
+
+}
+
+export default Map;
