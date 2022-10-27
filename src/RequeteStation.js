fe5651309079e651bbf5b15b6e5ef054376935c6
Julien Dessaign <juliendessaigne24@gmail.com>
2022-10-27 16:45:12 +0200

    récupération des données

--- src/RequeteStation.js
+++ src/RequeteStation.js
@@ -1,51 +1,19 @@
-class RequeteStation extends React.Component {
+import React from "react";
+
+
+export default class RequeteStation extends React.Component {
     constructor(props) {
-      super(props);
-      this.state = {
-        error: null,
-        isLoaded: false,
-        items: []
-      };
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            items: []
+        };
+        this.contract_name = this.props.contract_name
+        console.log(this.props);
     }
-  
+
     componentDidMount() {
-      fetch("https://.example.com/items")
-        .then(res => res.json())
-        .then(
-          (result) => {
-            this.setState({
-              isLoaded: true,
-              items: result.items
-            });
-          },
-          // Note: it's important to handle errors here
-          // instead of a catch() block so that we don't swallow
-          // exceptions from actual bugs in components.
-          (error) => {
-            this.setState({
-              isLoaded: true,
-              error
-            });
-          }
-        )
+        console.log(this.contract_name);
     }
-  
-    render() {
-      const { error, isLoaded, items } = this.state;
-      if (error) {
-        return <div>Error: {error.message}</div>;
-      } else if (!isLoaded) {
-        return <div>Loading...</div>;
-      } else {
-        return (
-          <ul>
-            {items.map(item => (
-              <li key={item.id}>
-                {item.name} {item.price}
-              </li>
-            ))}
-          </ul>
-        );
-      }
-    }
-  }
+}
